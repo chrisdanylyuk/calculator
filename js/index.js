80d043410125d70404b90f95a5e6682d7634279e
chrisdanylyuk <christina.danylyuk@gmail.com>
2020-05-09 17:38:12 +0000

    Sample Code

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const display = document.querySelector('.display');
+
+document.querySelectorAll('.digits button')
+.forEach(button => button.addEventListener('click', digitPressed));
+
+function digitPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelectorAll('.opers button')
+.forEach(button => button.addEventListener('click', opersPressed));
+
+function opersPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelector('.eq').addEventListener('click', calculate);
+
+function calculate() {
+    display.value = eval(display.value);
+}
+
+document.querySelector('.reset').addEventListener('click', reset);
+function reset() {
+    display.value = " ";
+}
+
+document.querySelector('.point').addEventListener('click', point);
+function point() {
+    display.value = display.value + ".";
+}
+
+
+
+
+
+
+
